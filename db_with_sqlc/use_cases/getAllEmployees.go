82ba49dbd4099f16fe2b13065e0146f64ab3ff6f
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-06-16 14:21:13 -0300

    config: GetEmployees function name updated

--- db_with_sqlc/use_cases/getAllEmployees.go
+++ db_with_sqlc/use_cases/getAllEmployees.go
@@ -7,7 +7,7 @@ import (
 	database "github.com/GuiFernandess7/db_with_sqlc/db"
 )
 
-func GetEmployees(limit int32, db_repository *database.Queries, ctx context.Context)([]database.Employee, error){
+func GetEmployees(limit int32, db_repository *database.Queries, ctx context.Context)([]database.ListAllEmployeesRow, error){
 	result, err := db_repository.ListAllEmployees(ctx)
 	if err != nil {
 		log.Fatalf("error getting all employees: %v", err)
