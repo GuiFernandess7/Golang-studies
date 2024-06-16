433d5c54cc702dbf1878c21bbea6c486c04b3430
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-06-16 14:20:32 -0300

    feat: New Queries Added

--- db_with_sqlc/query.sql
+++ db_with_sqlc/query.sql
@@ -1,8 +1,16 @@
 -- name: ListAllEmployees :many
-SELECT * FROM employee;
+-- Get all employees including their branches
+SELECT *
+FROM employee
+LEFT JOIN branch
+ON employee.emp_id = branch.mgr_id;
 
 -- name: ListAllEmployeesBy :many
-SELECT * FROM employee
+-- Get All employees by limit in order desc
+SELECT *
+FROM employee
+LEFT JOIN branch
+ON employee.emp_id = branch.mgr_id
 ORDER BY emp_id DESC
 LIMIT $1;
 
@@ -52,7 +60,32 @@ FROM employee
 JOIN branch
 ON employee.emp_id = branch.mgr_id;
 
--- name: ListClients :many
-SELECT * FROM client;
+-- name: GetClientbyBranch :many
+SELECT client.client_id, client_name, branch.branch_name
+FROM client
+JOIN branch
+ON branch.branch_id = client.branch_id;
 
+-- name: GetEmployeeBySalesQtd :many
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+    SELECT works_with.emp_id
+    FROM works_with
+    WHERE works_with.total_sales > @min::INT
+    AND works_with.total_sales < @max::INT
+);
 
+-- name: GetClientsByEmployee :many
+SELECT c.client_name
+FROM client c
+JOIN branch b ON c.branch_id = b.branch_id
+JOIN employee e ON b.mgr_id = e.emp_id
+WHERE e.first_name = @first_name::VARCHAR;
+
+-- name: GetSupplierByType :many
+SELECT branch_supplier.supplier_name, branch.branch_name, branch_supplier.supply_type
+FROM branch_supplier
+JOIN branch
+ON branch_supplier.branch_id = branch.branch_id
+WHERE branch_supplier.supply_type = @type::VARCHAR;
